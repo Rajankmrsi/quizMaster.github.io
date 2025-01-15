6ca5830c0e0dcf1d574ec39664960c9980fe0174
Rajankmrsi <cpsrajan2002@gmail.com>
2025-01-15 16:51:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,318 @@
+const questions=[
+ {
+    question:"Which is largest animal in the world?",
+    answers:[
+        { text:"shark",correct:false},
+        { text:"blue whale",correct:true},
+        { text:"elephant",correct:false},
+        { text:"giraffe",correct:false},
+    ]
+ },
+ {
+    question:"What is the capital of india?",
+    answers:[
+        { text:"mumbai",correct:false},
+        { text:"new delhi",correct:true},
+        { text:"kolkata",correct:false},
+        { text:"bengaluru",correct:false},
+    ]
+ },
+ {
+    question:"Who was the first prime minister of india?",
+    answers:[
+        { text:"mahatma gandhi",correct:false},
+        { text:"jawaharlal nehru",correct:true},
+        { text:"Rajendra prasad",correct:false},
+        { text:"lal bahdur sastri",correct:false},
+    ]
+ },
+ {
+    question:"What is the national sport of india?",
+    answers:[
+        { text:"cricket",correct:false},
+        { text:"hockey",correct:true},
+        { text:"football",correct:false},
+        { text:"kabadi",correct:false},
+    ]
+ },
+
+
+
+    {
+        question: 'What is the capital of France?',
+        answers: [
+            { text: 'Berlin', correct: false },
+            { text: 'Madrid', correct: false },
+            { text: 'Paris', correct: true },
+            { text: 'Lisbon', correct: false },
+        ]
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        answers: [
+            { text: 'Earth', correct: false },
+            { text: 'Mars', correct: true },
+            { text: 'Jupiter', correct: false },
+            { text: 'Saturn', correct: false },
+        ]
+    },
+    {
+        question: 'What is the largest ocean on Earth?',
+        answers: [
+            { text: 'Atlantic Ocean', correct: false },
+            { text: 'Indian Ocean', correct: false },
+            { text: 'Arctic Ocean', correct: false },
+            { text: 'Pacific Ocean', correct: true },
+        ]
+    },
+    {
+        question: 'What is the chemical symbol for gold?',
+        answers: [
+            { text: 'Au', correct: true },
+            { text: 'Ag', correct: false },
+            { text: 'Fe', correct: false },
+            { text: 'Pb', correct: false },
+        ]
+    },
+    
+    {
+        question: 'Which gas do plants absorb from the atmosphere?',
+        answers: [
+            { text: 'Oxygen', correct: false },
+            { text: 'Carbon Dioxide', correct: true },
+            { text: 'Nitrogen', correct: false },
+            { text: 'Hydrogen', correct: false },
+        ]
+    },
+    {
+        question: 'which is the national flower of india',
+        answers: [
+            { text: 'Lotus', correct: true },
+            { text: 'Rose', correct: false },
+            { text: 'Jasmine', correct: false },
+            { text: 'Sunflower', correct: false },
+        ]
+    },
+    {
+        question: 'Which river is considered the holiest in india',
+        answers: [
+            { text: 'Ganga', correct: true },
+            { text: 'Yamuna', correct: false },
+            { text: 'Brahmaputra', correct: false },
+            { text: 'Godavri', correct: false },
+        ]
+    },
+    {
+        question: 'what is the national animal of india',
+        answers: [
+            { text: 'Lion', correct: false },
+            { text: 'Peacock', correct: false },
+            { text: 'Tiger', correct: true },
+            { text: 'Elephant', correct: false },
+        ]
+    },
+    {
+        question: 'Who is known as the father of national in india',
+        answers: [
+            { text: 'Jawaharlal Nehru', correct: false },
+            { text: 'Subhash Chandra Bose', correct: false },
+            { text: 'Sardar Patel', correct: false },
+            { text: 'Mahatma Gandhi', correct: true },
+        ]
+    },
+    {
+        question: 'Which is the largest state in india by area?',
+        answers: [
+            { text: 'Madhya Pradesh', correct: false },
+            { text: 'Rajasthan', correct: true },
+            { text: 'Maharashtra', correct: false },
+            { text: 'Uttar Pradesh', correct: false },
+        ]
+    },
+    {
+        question: 'In which year did india gain independence?',
+        answers: [
+            { text: '1946', correct: false },
+            { text: '1950', correct: false },
+            { text: '1958', correct: false },
+            { text: '1947', correct: true },
+        ]
+    },
+    {
+        question: 'Which festival is known as the festival of lights?',
+        answers: [
+            { text: 'Diwali', correct: true },
+            { text: 'Holi', correct: false },
+            { text: 'Dussehra', correct: false },
+            { text: 'Pongal', correct: false },
+        ]
+    },
+    {
+        question: 'What is the national bird of India?',
+        answers: [
+            { text: 'Crow', correct: false },
+            { text: 'Parrot', correct: false },
+            { text: 'Peacock', correct: true },
+            { text: 'Sparrow', correct: false },
+        ]
+    },
+    {
+        question: 'Which monoment is known as the symbol of love?',
+        answers: [
+            { text: 'Charminar', correct: false },
+            { text: 'Qutub Minar', correct: false },
+            { text: 'India Gate', correct: false },
+            { text: 'Taj Mahal', correct: true },
+        ]
+    },
+    {
+        question: 'Which is the national currency of india?',
+        answers: [
+            { text: 'Dolar', correct: false },
+            { text: 'Rupee', correct: true },
+            { text: 'Yen', correct: false },
+            { text: 'Pound', correct: false },
+        ]
+    },
+    {
+        question: 'Who wrote the National Anthem?',
+        answers: [
+            { text: 'Rabindranath Tagore', correct: true },
+            { text: 'Bankim Chandra Chatterjee', correct: false },
+            { text: 'Sarojni Naidu', correct: false },
+            { text: 'Subramania Bharati', correct: false },
+        ]
+    },
+    {
+        question: 'Which is the smallest state in india?',
+        answers: [
+            { text: 'Sikkim', correct: false },
+            { text: 'Goa', correct: true },
+            { text: 'Tripura', correct: false },
+            { text: 'Nagaland', correct: false },
+        ]
+    },
+    {
+        question: 'Which city is known as the silicon valley of india?',
+        answers: [
+            { text: 'Chennai', correct: false },
+            { text: 'Pune', correct: false },
+            { text: 'Hyderabad', correct: false },
+            { text: 'Bengaluru', correct: true },
+        ]
+    },
+    
+    {
+        question: 'Which india city is known as the pink city?',
+        answers: [
+            { text: 'Jaipur', correct: true },
+            { text: 'Jodhpur', correct: false },
+            { text: 'Udaipur', correct: false },
+            { text: 'Bikaner', correct: false },
+        ]
+    },
+    {
+        question: 'Which Indian state is famous for its tea gardens?',
+        answers: [
+            { text: 'Kerala', correct: false },
+            { text: 'Assam', correct: true },
+            { text: 'West Bengal', correct: false },
+            { text: 'Tamil Naidu', correct: false },
+        ]
+    },
+    {
+        question: 'Which is the oldest mountain range in india?',
+        answers: [
+            { text: 'Satpura Range', correct: false },
+            { text: 'Vindhya Range', correct: false },
+            { text: 'Himalays', correct: false },
+            { text: 'Aravalli Range', correct: true },
+        ]
+    },
+];
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton =document.getElementById("next-btn");
+
+let currentQuestionIndex=0;
+let score =0;
+
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0;
+    nextButton.innerHTML="Next";
+    showQuestion();
+}
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo= currentQuestionIndex + 1;
+    questionElement.innerHTML= questionNo +" ." + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button =document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct=answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+ function resetState(){
+    nextButton.style.display="none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+ }
+
+ function selectAnswer(e){
+    const selectedBtn=e.target;
+    const isCorrect=selectedBtn.dataset.correct ==="true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button =>{
+        if(button.dataset.correct ==="true")
+        {
+            button.classList.add("correct");
+        }
+        button.disabled ="true";
+    });
+    nextButton.style.display="block";
+ }
+function showScore(){
+    resetState();
+    questionElement.innerHTML=`you scored ${score} out of ${questions.length}`;
+    nextButton.innerHTML="Play Again";
+    nextButton.style.display="block";
+}
+
+ function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+ }
+
+ 
+ nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+ })
+
+
+startQuiz();
+
